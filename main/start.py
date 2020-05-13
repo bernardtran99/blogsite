b8ca1b0a1c5951c252773823114fb0fb6bbfb257
bernardtran99 <bernardtran99@gmail.com>
2020-05-13 11:39:48 -0400

    Update start.py

--- main/start.py
+++ main/start.py
@@ -26,6 +26,8 @@ def start():
             time.sleep(1)
             break
         elif int(num) == 2:
+            print("Goodbye! :)")
+            time.sleep(1)
             exit()
         else:
             print("Please enter a valid number:")
@@ -36,7 +38,39 @@ def start():
     print(".....")
     time.sleep(1)
     print(".....")
+    print("\n")
 
-    print("")
+    print("Chapter 1:")
+    time.sleep(3)
+    print("There once was a young man named, " + name + ", who had discovered a computer for the first time.")
+    time.sleep(3)
+    print(name + " was so enticed by the coputer's bright lights and funny noises.")
+    time.sleep(3)
+    print("He immediatly grabbed onto the mouse and layed his fingers down onto the keyboard.")
+    time.sleep(3)
+    print("There was an intriguing icon on the desktop that caught " + name + "'s attention.")
+    time.sleep(3)
+    print('"Game World Online", he read to himself.')
+    time.sleep(3)
+    print("Does " + name + " click on the icon?")
+    print("1: Yes \n2: No")
+    num = input()
+
+    while True:
+        if int(num) == 1:
+            time.sleep(1)
+            break
+        elif int(num) == 2:
+            print("You suddenly feel a sense of regret that you did not click on the game.")
+            time.sleep(3)
+            print("You walk away slow and sad.")
+            time.sleep(3)
+            exit()
+        else:
+            print("Please enter a valid number:")
+            num = input()
+    
+    print("As you click on the game, your whole body is suddenly envoloped in bright lights.")
+
 
 start()
